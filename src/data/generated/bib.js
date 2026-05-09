2f5d900189698c8ffc0043f096d15d3a27a164ca
Bobby <bobby.newman@outlook.com>
2026-05-09 17:55:36 +0100

    First commit.

---
+++ src/data/generated/bib.js
@@ -0,0 +1,40 @@
+﻿define({ entries : {
+    "debattista2018": {
+        "abstract": "Abstract Maximizing performance for rendered content requires making compromises on quality parameters depending on the computational resources available . Yet, it is currently unclear which parameters best maximize perceived quality. This work investigates perceived quality across computational budgets for the primary spatiotemporal parameters of resolution and frame rate. Three experiments are conducted. Experiment 1 (n",
+        "author": "Debattista, K. and Bugeja, K. and Spina, S. and Bashford-Rogers, T. and Hulusic, V.",
+        "doi": "https://doi.org/10.1111/cgf.13302",
+        "eprint": "https://onlinelibrary.wiley.com/doi/pdf/10.1111/cgf.13302",
+        "journal": "Computer Graphics Forum",
+        "keywords": "perceptually-based rendering, human factors, real-time rendering, I.3.3 Computer Graphics: Picture/Image Generation\u2014Line and curve generation",
+        "number": "1",
+        "pages": "363-374",
+        "title": "Frame Rate vs Resolution: A Subjective Evaluation of Spatiotemporal Perceived Quality Under Varying Computational Budgets",
+        "type": "article",
+        "url": "https://onlinelibrary.wiley.com/doi/abs/10.1111/cgf.13302",
+        "volume": "37",
+        "year": "2018"
+    },
+    "mori2012": {
+        "author": "Mori, Masahiro and MacDorman, Karl F. and Kageki, Norri",
+        "doi": "10.1109/MRA.2012.2192811",
+        "journal": "IEEE Robotics \\& Automation Magazine",
+        "keywords": "",
+        "number": "2",
+        "pages": "98-100",
+        "title": "The Uncanny Valley [From the Field]",
+        "type": "article",
+        "volume": "19",
+        "year": "2012"
+    },
+    "navarro2020": {
+        "author": "Navarro-Hinojosa, Octavio and Alencastre-Miranda, Mois{\\'e}s",
+        "journal": "Applied Sciences",
+        "number": "6",
+        "pages": "2099",
+        "publisher": "MDPI",
+        "title": "Simulation of skeletal muscles in real-time with parallel computing in GPU",
+        "type": "article",
+        "volume": "10",
+        "year": "2020"
+    }
+}});
